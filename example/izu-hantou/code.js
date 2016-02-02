fd3480fbc7cca534a5a9a997e0fd82b3d02d085c
Minoru TOMOBE <minoru.tomobe@gmail.com>
2016-02-03 03:27:44 +0900

    new example

---
+++ example/izu-hantou/code.js
@@ -0,0 +1,562 @@
+function map_canvas() {
+ var data_peak = new Array();
+ var data_saddle = new Array();
+ var polylines = new Array();
+data_peak.push({
+lat: 3.5333333333e+01,
+lng: 1.3873522222e+02,
+cert : false,
+content:' Peak = 2261.800049 pos = 35.3333,138.7352 diff = 12260.799805'
+});
+data_saddle.push({
+lat: 3.5115444445e+01,
+lng: 1.3862500000e+02,
+content:'Saddle = -9999.000000 pos = 35.1154,138.6250 diff = 12260.799805'
+});
+data_peak.push({
+lat: 3.5092333334e+01,
+lng: 1.3887755556e+02,
+cert : false,
+content:' Peak = 192.500000 pos = 35.0923,138.8776 diff = 165.399994'
+});
+data_saddle.push({
+lat: 3.5089000001e+01,
+lng: 1.3888500000e+02,
+content:'Saddle = 27.100000 pos = 35.0890,138.8850 diff = 165.399994'
+});
+data_peak.push({
+lat: 3.5316333333e+01,
+lng: 1.3919777778e+02,
+cert : false,
+content:' Peak = 326.700012 pos = 35.3163,139.1978 diff = 292.100006'
+});
+data_saddle.push({
+lat: 3.5333222222e+01,
+lng: 1.3913533333e+02,
+content:'Saddle = 34.599998 pos = 35.3332,139.1353 diff = 292.100006'
+});
+data_peak.push({
+lat: 3.5059444445e+01,
+lng: 1.3889388889e+02,
+cert : true,
+content:'Name = JA/SO-115(JA/SO-115) peak = 391.700012 pos = 35.0594,138.8939 diff = 349.200012'
+});
+data_saddle.push({
+lat: 3.5047444445e+01,
+lng: 1.3890844444e+02,
+content:'Saddle = 42.500000 pos = 35.0474,138.9084 diff = 349.200012'
+});
+data_peak.push({
+lat: 3.5046444445e+01,
+lng: 1.3892577778e+02,
+cert : false,
+content:' Peak = 206.500000 pos = 35.0464,138.9258 diff = 161.300003'
+});
+data_saddle.push({
+lat: 3.5043000001e+01,
+lng: 1.3892122222e+02,
+content:'Saddle = 45.200001 pos = 35.0430,138.9212 diff = 161.300003'
+});
+data_peak.push({
+lat: 3.4655333336e+01,
+lng: 1.3889755556e+02,
+cert : false,
+content:' Peak = 200.199997 pos = 34.6553,138.8976 diff = 152.399994'
+});
+data_saddle.push({
+lat: 3.4665666669e+01,
+lng: 1.3890000000e+02,
+content:'Saddle = 47.799999 pos = 34.6657,138.9000 diff = 152.399994'
+});
+data_peak.push({
+lat: 3.4673555558e+01,
+lng: 1.3892655556e+02,
+cert : false,
+content:' Peak = 252.000000 pos = 34.6736,138.9266 diff = 150.899994'
+});
+data_saddle.push({
+lat: 3.4680222225e+01,
+lng: 1.3892544444e+02,
+content:'Saddle = 101.099998 pos = 34.6802,138.9254 diff = 150.899994'
+});
+data_peak.push({
+lat: 3.4635111114e+01,
+lng: 1.3883788889e+02,
+cert : true,
+content:'Name = JA/SO-119(JA/SO-119) peak = 307.899994 pos = 34.6351,138.8379 diff = 200.599991'
+});
+data_saddle.push({
+lat: 3.4635333336e+01,
+lng: 1.3881466667e+02,
+content:'Saddle = 107.300003 pos = 34.6353,138.8147 diff = 200.599991'
+});
+data_peak.push({
+lat: 3.5036222223e+01,
+lng: 1.3891211111e+02,
+cert : true,
+content:'Name = JA/SO-122(JA/SO-122) peak = 272.399994 pos = 35.0362,138.9121 diff = 164.699997'
+});
+data_saddle.push({
+lat: 3.5025222223e+01,
+lng: 1.3890888889e+02,
+content:'Saddle = 107.699997 pos = 35.0252,138.9089 diff = 164.699997'
+});
+data_peak.push({
+lat: 3.4819000002e+01,
+lng: 1.3875900000e+02,
+cert : true,
+content:'Name = JA/SO-121(JA/SO-121) peak = 302.100006 pos = 34.8190,138.7590 diff = 193.300003'
+});
+data_saddle.push({
+lat: 3.4815666668e+01,
+lng: 1.3876844444e+02,
+content:'Saddle = 108.800003 pos = 34.8157,138.7684 diff = 193.300003'
+});
+data_peak.push({
+lat: 3.4939666668e+01,
+lng: 1.3913133333e+02,
+cert : false,
+content:' Peak = 320.899994 pos = 34.9397,139.1313 diff = 154.099991'
+});
+data_saddle.push({
+lat: 3.4932111112e+01,
+lng: 1.3912733333e+02,
+content:'Saddle = 166.800003 pos = 34.9321,139.1273 diff = 154.099991'
+});
+data_peak.push({
+lat: 3.4702888891e+01,
+lng: 1.3895522222e+02,
+cert : true,
+content:'Name = JA/SO-118(JA/SO-118) peak = 342.100006 pos = 34.7029,138.9552 diff = 165.600006'
+});
+data_saddle.push({
+lat: 3.4721777780e+01,
+lng: 1.3896755556e+02,
+content:'Saddle = 176.500000 pos = 34.7218,138.9676 diff = 165.600006'
+});
+data_peak.push({
+lat: 3.4732111113e+01,
+lng: 1.3876955556e+02,
+cert : false,
+content:' Peak = 357.700012 pos = 34.7321,138.7696 diff = 170.000015'
+});
+data_saddle.push({
+lat: 3.4727888891e+01,
+lng: 1.3876866667e+02,
+content:'Saddle = 187.699997 pos = 34.7279,138.7687 diff = 170.000015'
+});
+data_peak.push({
+lat: 3.4677333336e+01,
+lng: 1.3881022222e+02,
+cert : false,
+content:' Peak = 352.899994 pos = 34.6773,138.8102 diff = 155.000000'
+});
+data_saddle.push({
+lat: 3.4681555558e+01,
+lng: 1.3879777778e+02,
+content:'Saddle = 197.899994 pos = 34.6816,138.7978 diff = 155.000000'
+});
+data_peak.push({
+lat: 3.5011777779e+01,
+lng: 1.3892022222e+02,
+cert : true,
+content:'Name = JA/SO-108(JA/SO-108) peak = 451.000000 pos = 35.0118,138.9202 diff = 235.100006'
+});
+data_saddle.push({
+lat: 3.5001222223e+01,
+lng: 1.3889022222e+02,
+content:'Saddle = 215.899994 pos = 35.0012,138.8902 diff = 235.100006'
+});
+data_peak.push({
+lat: 3.4742666669e+01,
+lng: 1.3895788889e+02,
+cert : false,
+content:' Peak = 454.100006 pos = 34.7427,138.9579 diff = 211.400009'
+});
+data_saddle.push({
+lat: 3.4758222224e+01,
+lng: 1.3896511111e+02,
+content:'Saddle = 242.699997 pos = 34.7582,138.9651 diff = 211.400009'
+});
+data_peak.push({
+lat: 3.4707111113e+01,
+lng: 1.3875177778e+02,
+cert : true,
+content:'Name = JA/SO-097(JA/SO-097) peak = 517.200012 pos = 34.7071,138.7518 diff = 246.500000'
+});
+data_saddle.push({
+lat: 3.4707111113e+01,
+lng: 1.3875833333e+02,
+content:'Saddle = 270.700012 pos = 34.7071,138.7583 diff = 246.500000'
+});
+data_peak.push({
+lat: 3.4795555557e+01,
+lng: 1.3878155556e+02,
+cert : false,
+content:' Peak = 435.299988 pos = 34.7956,138.7816 diff = 154.799988'
+});
+data_saddle.push({
+lat: 3.4798111113e+01,
+lng: 1.3878444444e+02,
+content:'Saddle = 280.500000 pos = 34.7981,138.7844 diff = 154.799988'
+});
+data_peak.push({
+lat: 3.4703555558e+01,
+lng: 1.3885511111e+02,
+cert : true,
+content:'Name = JA/SO-092(JA/SO-092) peak = 543.099976 pos = 34.7036,138.8551 diff = 231.099976'
+});
+data_saddle.push({
+lat: 3.4731444447e+01,
+lng: 1.3885788889e+02,
+content:'Saddle = 312.000000 pos = 34.7314,138.8579 diff = 231.099976'
+});
+data_peak.push({
+lat: 3.4716444447e+01,
+lng: 1.3877722222e+02,
+cert : false,
+content:' Peak = 521.799988 pos = 34.7164,138.7772 diff = 183.099976'
+});
+data_saddle.push({
+lat: 3.4705000002e+01,
+lng: 1.3880122222e+02,
+content:'Saddle = 338.700012 pos = 34.7050,138.8012 diff = 183.099976'
+});
+data_peak.push({
+lat: 3.4903111112e+01,
+lng: 1.3909466667e+02,
+cert : true,
+content:'Name = Oomuroyama(JA/SO-082) peak = 580.000000 pos = 34.9031,139.0947 diff = 227.899994'
+});
+data_saddle.push({
+lat: 3.4906000001e+01,
+lng: 1.3908066667e+02,
+content:'Saddle = 352.100006 pos = 34.9060,139.0807 diff = 227.899994'
+});
+data_peak.push({
+lat: 3.4796666668e+01,
+lng: 1.3881711111e+02,
+cert : true,
+content:'Name = JA/SO-093(JA/SO-093) peak = 544.799988 pos = 34.7967,138.8171 diff = 191.899994'
+});
+data_saddle.push({
+lat: 3.4795222224e+01,
+lng: 1.3882555556e+02,
+content:'Saddle = 352.899994 pos = 34.7952,138.8256 diff = 191.899994'
+});
+data_peak.push({
+lat: 3.4862888890e+01,
+lng: 1.3900166667e+02,
+cert : true,
+content:'Name = Amagisan (Banzaburoudake)(JA/SO-030) peak = 1404.699951 pos = 34.8629,139.0017 diff = 1046.799927'
+});
+data_saddle.push({
+lat: 3.4952000001e+01,
+lng: 1.3905355556e+02,
+content:'Saddle = 357.899994 pos = 34.9520,139.0536 diff = 1046.799927'
+});
+data_peak.push({
+lat: 3.4737888891e+01,
+lng: 1.3885944444e+02,
+cert : true,
+content:'Name = JA/SO-079(JA/SO-079) peak = 606.799988 pos = 34.7379,138.8594 diff = 195.099976'
+});
+data_saddle.push({
+lat: 3.4742444446e+01,
+lng: 1.3887044444e+02,
+content:'Saddle = 411.700012 pos = 34.7424,138.8704 diff = 195.099976'
+});
+data_peak.push({
+lat: 3.4927888890e+01,
+lng: 1.3896444444e+02,
+cert : true,
+content:'Name = JA/SO-078(JA/SO-078) peak = 607.500000 pos = 34.9279,138.9644 diff = 190.000000'
+});
+data_saddle.push({
+lat: 3.4909111112e+01,
+lng: 1.3895300000e+02,
+content:'Saddle = 417.500000 pos = 34.9091,138.9530 diff = 190.000000'
+});
+data_peak.push({
+lat: 3.4817333335e+01,
+lng: 1.3879433333e+02,
+cert : true,
+content:'Name = JA/SO-069(JA/SO-069) peak = 702.599976 pos = 34.8173,138.7943 diff = 216.299988'
+});
+data_saddle.push({
+lat: 3.4822222224e+01,
+lng: 1.3881688889e+02,
+content:'Saddle = 486.299988 pos = 34.8222,138.8169 diff = 216.299988'
+});
+data_peak.push({
+lat: 3.4955111112e+01,
+lng: 1.3883922222e+02,
+cert : true,
+content:'Name = Darumayama(JA/SO-051) peak = 981.000000 pos = 34.9551,138.8392 diff = 403.099976'
+});
+data_saddle.push({
+lat: 3.4922111112e+01,
+lng: 1.3884144444e+02,
+content:'Saddle = 577.900024 pos = 34.9221,138.8414 diff = 403.099976'
+});
+data_peak.push({
+lat: 3.4894888890e+01,
+lng: 1.3905722222e+02,
+cert : false,
+content:' Peak = 815.000000 pos = 34.8949,139.0572 diff = 153.099976'
+});
+data_saddle.push({
+lat: 3.4894444446e+01,
+lng: 1.3905344444e+02,
+content:'Saddle = 661.900024 pos = 34.8944,139.0534 diff = 153.099976'
+});
+data_peak.push({
+lat: 3.4788444446e+01,
+lng: 1.3886800000e+02,
+cert : true,
+content:'Name = Chyoukurouyama(JA/SO-050) peak = 995.299988 pos = 34.7884,138.8680 diff = 263.200012'
+});
+data_saddle.push({
+lat: 3.4807666668e+01,
+lng: 1.3888144444e+02,
+content:'Saddle = 732.099976 pos = 34.8077,138.8814 diff = 263.200012'
+});
+data_peak.push({
+lat: 3.4884555557e+01,
+lng: 1.3884033333e+02,
+cert : false,
+content:' Peak = 932.599976 pos = 34.8846,138.8403 diff = 160.000000'
+});
+data_saddle.push({
+lat: 3.4872666668e+01,
+lng: 1.3883488889e+02,
+content:'Saddle = 772.599976 pos = 34.8727,138.8349 diff = 160.000000'
+});
+data_peak.push({
+lat: 3.4857777779e+01,
+lng: 1.3886366667e+02,
+cert : true,
+content:'Name = JA/SO-047(JA/SO-047) peak = 1035.500000 pos = 34.8578,138.8637 diff = 212.900024'
+});
+data_saddle.push({
+lat: 3.4831777779e+01,
+lng: 1.3890811111e+02,
+content:'Saddle = 822.599976 pos = 34.8318,138.9081 diff = 212.900024'
+});
+data_peak.push({
+lat: 3.4878888890e+01,
+lng: 1.3903244444e+02,
+cert : true,
+content:'Name = JA/SO-038(JA/SO-038) peak = 1196.300049 pos = 34.8789,139.0324 diff = 177.900024'
+});
+data_saddle.push({
+lat: 3.4874555557e+01,
+lng: 1.3902644444e+02,
+content:'Saddle = 1018.400024 pos = 34.8746,139.0264 diff = 177.900024'
+});
+data_peak.push({
+lat: 3.4971777779e+01,
+lng: 1.3905955556e+02,
+cert : false,
+content:' Peak = 593.400024 pos = 34.9718,139.0596 diff = 161.700012'
+});
+data_saddle.push({
+lat: 3.5017000001e+01,
+lng: 1.3904533333e+02,
+content:'Saddle = 431.700012 pos = 35.0170,139.0453 diff = 161.700012'
+});
+data_peak.push({
+lat: 3.5233444445e+01,
+lng: 1.3902077778e+02,
+cert : true,
+content:'Name = Hakoneyama (Kamiyama)(JA/KN-004) peak = 1437.400024 pos = 35.2334,139.0208 diff = 991.100037'
+});
+data_saddle.push({
+lat: 3.5293888889e+01,
+lng: 1.3894088889e+02,
+content:'Saddle = 446.299988 pos = 35.2939,138.9409 diff = 991.100037'
+});
+data_peak.push({
+lat: 3.5081777778e+01,
+lng: 1.3902800000e+02,
+cert : true,
+content:'Name = Kurotake(JA/SO-061) peak = 797.299988 pos = 35.0818,139.0280 diff = 180.299988'
+});
+data_saddle.push({
+lat: 3.5101222223e+01,
+lng: 1.3903500000e+02,
+content:'Saddle = 617.000000 pos = 35.1012,139.0350 diff = 180.299988'
+});
+data_peak.push({
+lat: 3.5328666666e+01,
+lng: 1.3903533333e+02,
+cert : true,
+content:'Name = Yaguradake(JA/KN-016) peak = 869.000000 pos = 35.3287,139.0353 diff = 217.000000'
+});
+data_saddle.push({
+lat: 3.5332000000e+01,
+lng: 1.3902822222e+02,
+content:'Saddle = 652.000000 pos = 35.3320,139.0282 diff = 217.000000'
+});
+data_peak.push({
+lat: 3.5289777778e+01,
+lng: 1.3900477778e+02,
+cert : true,
+content:'Name = Hakoneyama (Kintokizan)(JA/KN-007) peak = 1212.000000 pos = 35.2898,139.0048 diff = 423.700012'
+});
+data_saddle.push({
+lat: 3.5205777778e+01,
+lng: 1.3903566667e+02,
+content:'Saddle = 788.299988 pos = 35.2058,139.0357 diff = 423.700012'
+});
+data_peak.push({
+lat: 3.5195666667e+01,
+lng: 1.3903600000e+02,
+cert : false,
+content:' Peak = 948.000000 pos = 35.1957,139.0360 diff = 159.700012'
+});
+data_saddle.push({
+lat: 3.5186888889e+01,
+lng: 1.3903211111e+02,
+content:'Saddle = 788.299988 pos = 35.1869,139.0321 diff = 159.700012'
+});
+data_peak.push({
+lat: 3.5183111111e+01,
+lng: 1.3904611111e+02,
+cert : true,
+content:'Name = Daikanzan(JA/KN-014) peak = 1011.700012 pos = 35.1831,139.0461 diff = 164.100037'
+});
+data_saddle.push({
+lat: 3.5180777778e+01,
+lng: 1.3901366667e+02,
+content:'Saddle = 847.599976 pos = 35.1808,139.0137 diff = 164.100037'
+});
+data_peak.push({
+lat: 3.5216000000e+01,
+lng: 1.3898388889e+02,
+cert : true,
+content:'Name = Mikuniyama(JA/KN-010) peak = 1101.599976 pos = 35.2160,138.9839 diff = 248.500000'
+});
+data_saddle.push({
+lat: 3.5230555556e+01,
+lng: 1.3897877778e+02,
+content:'Saddle = 853.099976 pos = 35.2306,138.9788 diff = 248.500000'
+});
+data_peak.push({
+lat: 3.5279555555e+01,
+lng: 1.3905244444e+02,
+cert : true,
+content:'Name = Myoujingatake(JA/KN-009) peak = 1168.099976 pos = 35.2796,139.0524 diff = 300.599976'
+});
+data_saddle.push({
+lat: 3.5282333333e+01,
+lng: 1.3901177778e+02,
+content:'Saddle = 867.500000 pos = 35.2823,139.0118 diff = 300.599976'
+});
+data_peak.push({
+lat: 3.5270444444e+01,
+lng: 1.3898211111e+02,
+cert : false,
+content:' Peak = 1155.000000 pos = 35.2704,138.9821 diff = 150.700012'
+});
+data_saddle.push({
+lat: 3.5281000000e+01,
+lng: 1.3898888889e+02,
+content:'Saddle = 1004.299988 pos = 35.2810,138.9889 diff = 150.700012'
+});
+data_peak.push({
+lat: 3.5214777778e+01,
+lng: 1.3904188889e+02,
+cert : true,
+content:'Name = Uefutagosan(JA/KN-011) peak = 1099.000000 pos = 35.2148,139.0419 diff = 221.500000'
+});
+data_saddle.push({
+lat: 3.5217555556e+01,
+lng: 1.3903866667e+02,
+content:'Saddle = 877.500000 pos = 35.2176,139.0387 diff = 221.500000'
+});
+data_peak.push({
+lat: 3.5238111111e+01,
+lng: 1.3879400000e+02,
+cert : true,
+content:'Name = Ashitakayama (Echizendake)(JA/SO-027) peak = 1503.099976 pos = 35.2381,138.7940 diff = 616.099976'
+});
+data_saddle.push({
+lat: 3.5256000000e+01,
+lng: 1.3879333333e+02,
+content:'Saddle = 887.000000 pos = 35.2560,138.7933 diff = 616.099976'
+});
+data_peak.push({
+lat: 3.5223777778e+01,
+lng: 1.3881000000e+02,
+cert : true,
+content:'Name = JA/SO-029(JA/SO-029) peak = 1455.699951 pos = 35.2238,138.8100 diff = 202.399902'
+});
+data_saddle.push({
+lat: 3.5227222222e+01,
+lng: 1.3879811111e+02,
+content:'Saddle = 1253.300049 pos = 35.2272,138.7981 diff = 202.399902'
+});
+var latlng = new google.maps.LatLng(data_peak[0].lat, data_peak[0].lng);
+var opts = {
+  zoom: 13,
+  center: latlng,
+  mapTypeId: google.maps.MapTypeId.TERRAIN
+};
+var map = new google.maps.Map(document.getElementById("map"), opts);
+var markers_peak = new Array();
+var markers_saddle = new Array();
+for (i = 0; i < data_peak.length; i++) {
+  if(data_peak[i].cert) {
+    markers_peak[i] = new google.maps.Marker({
+        position: new google.maps.LatLng(data_peak[i].lat, data_peak[i].lng),
+        icon : 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
+        map: map
+    });} else {
+    markers_peak[i] = new google.maps.Marker({
+        position: new google.maps.LatLng(data_peak[i].lat, data_peak[i].lng),
+        map: map
+    });}
+    markerInfo(markers_peak[i], data_peak[i].content);
+}
+for (i = 0; i < data_saddle.length; i++) {
+    markers_saddle[i] = new google.maps.Marker({
+        position: new google.maps.LatLng(data_saddle[i].lat, data_saddle[i].lng),
+        opacity: 0.2,
+        map: map
+    });
+    markerInfo(markers_saddle[i], data_saddle[i].content);
+}
+for (i = 0; i < data_peak.length; i++) {
+polylines[i] = new google.maps.Polyline({
+   map: map,
+	strokeColor:"#00007f",
+    strokeOpacity:0.7,
+	strokeWeight:2,
+    path: [
+	    new google.maps.LatLng(data_peak[i].lat, data_peak[i].lng),
+	    new google.maps.LatLng(data_saddle[i].lat, data_saddle[i].lng)
+	    ]
+});
+region_rect = new google.maps.Rectangle({
+   map: map,
+	strokeColor:"#00007f",
+   strokeOpacity:1.0,
+	strokeWeight:8,
+   fillColor: '000000',
+   fillOpacity: 0,
+    bounds: {
+       north:35.3333,
+       south:34.5,
+       east:139.25,
+       west:138.625}});
+};
+}
+function markerInfo(marker, name) {
+google.maps.event.addListener(marker, 'click', function (event) {
+   new google.maps.InfoWindow({
+        content: name
+    }).open(marker.getMap(), marker);
+});
+}
+google.maps.event.addDomListener(window, 'load', map_canvas);
