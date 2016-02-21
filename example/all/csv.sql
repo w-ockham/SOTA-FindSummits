7a217b86a58d5a12b76215ac1f5cf3428acbad34
Minoru TOMOBE <minoru.tomobe@gmail.com>
2016-02-21 15:28:48 +0900

    fix duplicate lines

---
+++ example/all/csv.sql
@@ -0,0 +1,1 @@
+create table summitlist( code text, region text, pref text, addr text, name text,elevation text,feet text, lng text, lat text, lat2 text,lng2 text,point text, num text,dates text,expire text,last text, call text, sname text,selevation text, sdiff text,slng text, slat text,gsi text);
